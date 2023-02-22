f692e4a3e578f83c3be4c6e0e18a7450a997d715
prasis73 <neptech73@gmail.com>
2023-02-22 13:53:30 +0545

    message

---
+++ lib/history_model.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: non_constant_identifier_names
+
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class HistoryModel {
+  String phone;
+  String from;
+  String message;
+  Timestamp date;
+  String userId;
+
+
+  HistoryModel({
+    required this.phone,
+    required this.from,
+    required this.message,
+    required this.date,
+    required this.userId,
+
+  }); 
+
+  factory HistoryModel.fromJson(Map<String, dynamic> json) {
+    return HistoryModel(
+      phone: json["phone_no"],
+      from: json["from"],
+      message: json["message"],
+      date: json["date"],
+      userId: json["userId"],
+
+    );
+  }
+}
